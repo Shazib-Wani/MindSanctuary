540fcc12d721bb96a1cf6b1043cbc7ef7aa8f729
Shazib-Wani <shazib20cs202@newhorizonindia.edu>
2023-04-13 16:48:36 +0530

    cards

--- inter.js
+++ inter.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
     $('.service-link').click(function() {
-    $(this).find('.service-item').toggleClass('active');
+      $(this).find('.service-item').toggleClass('active');
+      $('.submit-button').show();
     });
+  
+    $('#submit-btn').click(function() {
+      // Handle submit button click event here
     });
+  });
+  
